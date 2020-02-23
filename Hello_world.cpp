2d9618d5540cc63d347e79666aa4a7d34720ba3b
fantasia85 <541799565@qq.com>
2020-02-23 14:46:17 +0800

    helloworld

---
+++ Hello_world.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "hello world!1" << std::endl;
+	std::cout << "hello world!2" << std::endl;
+	std::cout << "hello world!3" << std::endl;
+	return 0;
+}
